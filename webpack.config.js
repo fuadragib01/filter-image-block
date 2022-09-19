bc04a9aad41344f480007aac65ae8747acf723d9
Fuad Ragib <ragibfuad@gmail.com>
2022-09-19 17:47:15 +0600

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+
+const plugins = defaultConfig.plugins.filter(
+    (plugin) =>
+        plugin.constructor.name != "MiniCssExtractPlugin" &&
+        plugin.constructor.name != "CleanWebpackPlugin"
+);
+
+let allEntries = {
+    build: "./src/index.js",
+    "build/frontend": "./src/frontend.js",
+};
+
+const config = {
+    ...defaultConfig,
+    entry: allEntries,
+    output: {
+        path: path.resolve(__dirname),
+        filename: "[name]/index.js",
+    },
+    plugins: [
+        ...plugins,
+        new MiniCSSExtractPlugin({
+            filename: "build/style-index.css",
+        }),
+    ],
+};
+
+module.exports = config;
